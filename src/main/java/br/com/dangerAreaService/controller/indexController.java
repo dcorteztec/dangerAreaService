ced06cb950497beba127863b19ac82cec1f1b1b6
dcorteztec <dcorteztec@gmail.com>
2017-12-23 09:33:41 -0500

    commit index controller

---
+++ src/main/java/br/com/dangerAreaService/controller/indexController.java
@@ -0,0 +1,17 @@
+package br.com.dangerAreaService.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getTexto() {
+        return "FUNFOU!!!!";
+    }
+
+}
