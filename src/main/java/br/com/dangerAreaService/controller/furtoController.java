e17c4383fddf413d6ba96b174691aeb0f611315c
unknown <dcorteztec@gmail.com>
2018-01-01 22:40:35 -0200

    alteração de nomenclatura e criação de listBase

--- src/main/java/br/com/dangerAreaService/controller/furtoController.java
+++ src/main/java/br/com/dangerAreaService/controller/furtoController.java
@@ -1,6 +1,7 @@
 package br.com.dangerAreaService.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dangerAreaService.ControllerLogger;
+import br.com.dangerAreaService.service.DadosRouboFurtoCelularService;
 import br.com.dangerAreaService.service.ReadExcelCelularRouboFurto;
 import br.com.dangerAreaService.util.Constantes;
 import br.com.dangerAreaService.vo.DadosCelularSPVO;
@@ -17,19 +19,34 @@ import br.com.dangerAreaService.vo.DadosCelularSPVO;
 
 @RestController
 @RequestMapping(Constantes.ESTATISTICA_FURTO_CELULAR)
-public class GerarBaseCalc extends ControllerLogger{
+public class furtoController extends ControllerLogger{
 
 	@Autowired
 	private ReadExcelCelularRouboFurto readExcel;
+	@Autowired
+	private DadosRouboFurtoCelularService dadosRouboFurtoCelularService;
 	
 	@RequestMapping(value = "/gerarBase", method = RequestMethod.GET,produces= MediaType.APPLICATION_JSON_VALUE)
-	public List<DadosCelularSPVO> gerendoBase() {
-		
+	public List<DadosCelularSPVO> gerandoBase() {
+		 List<DadosCelularSPVO> ret = new ArrayList<>();
 		try {
-			return readExcel.readExcelFurto();
+			ret = readExcel.readExcelFurto();
 		} catch (IOException e) {
-			 logger.error("This is an error gerendoBase()");
+			 logger.error("This is an error gerandoBase()");
 		}
-		return null;
+		return ret;
 	}
+	
+
+	@RequestMapping(value = "/listBase", method = RequestMethod.GET,produces= MediaType.APPLICATION_JSON_VALUE)
+	public List<DadosCelularSPVO> listBase() {
+		 List<DadosCelularSPVO> ret = new ArrayList<>();
+		try {
+			ret = dadosRouboFurtoCelularService.findAll();
+		} catch (Exception e) {
+			 logger.error("This is an error listBase()");
+		}
+		return ret;
+	}
+	
 }
