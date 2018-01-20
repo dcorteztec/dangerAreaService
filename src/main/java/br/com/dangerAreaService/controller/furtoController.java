2a37ae923bc84b7b4ed004ed9020643c64dcc726
unknown <dcorteztec@gmail.com>
2018-01-20 16:32:50 -0200

    acertos rest

--- src/main/java/br/com/dangerAreaService/controller/furtoController.java
+++ src/main/java/br/com/dangerAreaService/controller/furtoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(Constantes.ESTATISTICA_FURTO_CELULAR)
+@CrossOrigin(origins = "http://localhost:4200")
 public class furtoController extends ControllerLogger{
 
 	@Autowired
